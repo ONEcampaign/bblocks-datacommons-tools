256d05510f3025b45f7330e916e9f7c7eed18b5d
Jorge Rivera <jorge.rivera@one.org>
2025-04-28 10:23:43 +0200

    rename

--- src/bblocks/datacommons_tools/__init__.py
+++ src/bblocks/datacommons_tools/__init__.py
@@ -1,4 +1,4 @@
 from importlib.metadata import version
-from .custom_data.config import DCConfigManager
+from .custom_data.dc_config import DCConfigManager
 
 __version__ = version("bblocks-datacommons-tools")
