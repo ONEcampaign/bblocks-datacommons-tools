309838b62db854e8a40894aebede33a31593dff1
Jorge Rivera <jorge.rivera@one.org>
2025-05-20 09:08:03 +0100

    predefined properties followed by custom (#40)

--- src/bblocks/datacommons_tools/custom_data/models/mcf.py
+++ src/bblocks/datacommons_tools/custom_data/models/mcf.py
@@ -48,7 +48,7 @@ class MCFNode(BaseModel):
 
         # Pull Node first, then sort for consistent ordering
         lines = [f"Node: {data.pop('Node')}"]
-        lines.extend(f"{k}: {v}" for k, v in sorted(data.items()))
+        lines.extend(f"{k}: {v}" for k, v in data.items())
 
         return "\n".join(lines) + "\n\n"
 
